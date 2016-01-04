77240c5eb92ef60342376d4901472c32a4ef2ee3
Alexey Plutalov <demiazz.py@gmail.com>
2016-01-04 15:47:44 +0300

    Use mu-loader

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,53 +1,17 @@
 ;;------------------------------------------------------------------------------
-;; Packages
+;; µ Loader
 ;;------------------------------------------------------------------------------
 
-(require 'package)
-
-(add-to-list 'package-archives
-             '("marmalade"    . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa"        . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(package-initialize)
-
-;; use-package
-
-(defconst use-package-directory
-  (expand-file-name "use-package" user-emacs-directory))
-
-(add-to-list 'load-path use-package-directory)
-
-(unless (require 'use-package nil 'noerror)
-  (let* ((log (get-buffer-create "*use-package-installation*"))
-         (git (or (executable-find "git")
-                  (error "Unable to find `git` executable")))
-         (url "https://github.com/jwiegley/use-package")
-
-         (process-connection-type nil)
-
-         (status
-          (call-process
-           git nil `(,log t) t "--no-pager" "clone" "-v" url use-package-directory)))
-
-    (if (zerop status)
-        (message "Package `use-package` successfully installed")
-      (error "Couldn't clone `use-package` from the Git repository `%s`" url))))
+(load (expand-file-name "mu.el" user-emacs-directory)
 
 ;;------------------------------------------------------------------------------
-;; Configuration file
+;; Configuration
 ;;------------------------------------------------------------------------------
 
-(load-file (expand-file-name "config.el" user-emacs-directory))
+;; config keys and values
 
 ;;------------------------------------------------------------------------------
-;; Layers
+;; Bootstrap
 ;;------------------------------------------------------------------------------
 
-(if (boundp 'demiazz/layers)
-    (dolist (layer demiazz/layers)
-      (load-file
-       (expand-file-name (format "layers/%s.el" layer)
-                         user-emacs-directory))))
+(µ/load-layers '())
